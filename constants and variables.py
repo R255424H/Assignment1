37cc336be9e68d960bc62574425a011fffac6621
R255424H <xv.cinderellaman@gmail.com>
2025-09-07 14:35:09 +0200

    commit

---
+++ constants and variables.py
@@ -0,0 +1,10 @@
+#constant
+CLASS_TEACHER = "Rabi"
+CURRENT_YEAR= 2025
+
+#Variable
+stud_name = "Logic"
+stud_class = 1.1
+
+print(CLASS_TEACHER, "is the teacher for ", stud_class, "class" "\n", stud_name, " is in class, ", stud_class)
+print("\n")
